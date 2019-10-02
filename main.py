4cd1886865152a5d787ece5545145d28bf25c06a
Felipe Arias <felipe2arias@hotmail.com>
2019-10-02 17:30:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,40 @@ def validarEntre(min, max, msj):
         val = int(input(msj))
     return val
 
+def validarMayorQue(min):
+    n = int(input("Ingrese la cantidad de publicaciones a buscar: "))
+    while n <= min:
+        print("Error!")
+        n = int(input("Ingrese una cantidad valida: "))
+    return n
+
+def crearVector(n):
+    vec = [None] * n
+    for i in range(len(vec)):
+        codigo = random.randint(1, 100000)
+        precio = round(random.uniform(1, 100000), 2)
+        ubicacion = random.randint(1, 23)
+        estado = random.randint(0, 1)
+        cantidad = random.randint(0, 1000)
+        puntuacion = random.randint(1, 5)
+        publicacion = Publicacion(codigo, precio, ubicacion, estado, cantidad, puntuacion)
+        vec[i] = publicacion
+    return vec
+
+def mostrarVector(vec):
+    for pub in vec:
+        write(pub)
 
 def test():
     ban = False
     op = 0
     while op != 6:
+        n = validarMayorQue(0)
+        vec = crearVector(n)
+        mostrarVector(vec)
+        print()
         TGREEN =  '\033[32m' # TEXTO VERDE
-        ENDC = '\033[m' # reset to the defaults
+        ENDC = '\033[m' # TEXTO DEFAULT
         print(TGREEN + "Sitio de compra/venta 2.0 " + ENDC)
         menu()
         op = validarEntre(1, 6, "Ingrese una opcion: ")
@@ -44,4 +71,3 @@ def test():
 
 if __name__ == "__main__":
     test()
-
