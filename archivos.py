23f4b74fbbb3be18c637a0cd04f4507075abd093
Felipe Arias <felipe2arias@hotmail.com>
2019-10-04 18:02:21 -0300

    Update archivos.py

--- archivos.py
+++ archivos.py
@@ -1,7 +1,7 @@
-
 import os.path
+import io
 import pickle
-
+from main import *
 
 class Compra:
     def __init__(self, codigo, Ccomprada, precio, tipo, montoF, fecha):
@@ -13,14 +13,14 @@ class Compra:
         self.fecha = fecha
 
 def display(compra):
-    print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+    print("-" * 165)
     renglon = ""
     renglon += "Codigo: "'{:<10}'.format(compra.codigo)
-    renglon += "antidad comprada: ""$" '{:<20}'.format(compra.Ccomprada)
+    renglon += "Cantidad comprada: ""$" '{:<10}'.format(compra.Ccomprada)
     renglon += "Precio: "'{:<20}'.format(compra.precio)
-    renglon += "Tipo de envio: "'{:<20}'.format(compra.tipo)
+    renglon += "Tipo de envio: "'{:<10}'.format(compra.tipo)
     renglon += "Monto total abonado: "'{:<20}'.format(compra.montoF)
-    renglon += "Fecha: "'{:<20}'.format(compra.fecha)
+    renglon += "Fecha: "'{:<10}'.format(compra.fecha)
     print(renglon)
 
 def leerArchivo(FD):
@@ -33,8 +33,18 @@ def leerArchivo(FD):
         display(pub)
     m.close()
 
+
 def grabarVector(reg, arch):
-    m = open(arch, 'wb')
+    m = open(arch, 'ab')
     pickle.dump(reg, m)
     m.close()
+
+def generarArchivoTexto(reg):
+    m = open('miscompras.txt', 'at')
+    m.write('\n-----------------------------------------------')
+    m.write('\nCompra: {} - {} '.format(reg.codigo, reg.fecha))
+    m.write('\nResumen de compra:')
+    m.write('\nProducto: ${} ({} x ${})'.format(round(reg.precio, 2)* reg.Ccomprada, reg.Ccomprada, round(reg.precio, 2)))
+    m.write('\nCargo de envio: ${}'.format(round(((reg.precio * reg.Ccomprada)*0.10), 2)))
+    m.write('\nTu pago: ${}'.format(round(((reg.precio * reg.Ccomprada)*0.10) + reg.precio * reg.Ccomprada), 2))
 
