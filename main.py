5c4fce97d21c6e3fd3d64cf2baf0ca997f8cf486
belenyammal <56051839+belenyammal@users.noreply.github.com>
2019-10-06 16:59:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -173,8 +173,10 @@ def test():
                 generarArchivoTexto(reg)
                 leerArchivo(FD)
         if op == 2:
-            f1 = int(input("ingrese la primera fecha mas vieja:(formato aaaa/mm/dd)"))
-            f2 = int(input("ingrese la segunda fecha mas nueva:(formato aaaa/mm/dd)"))
+            y1, m1, d1 = [int(x) for x in input("ingrese la primera fecha(aaaammdd) : ").split('/')]
+            y2, m2, d2 = [int(x) for x in input("ingrese la segunda fecha(aaaammdd) : ").split('/')]
+            f1 = y1, m1, d1
+            f2 = y2, m2, d2
             mostrar_archivo_fechas(FD, f1, f2)
         if op == 3:
            rango(vec)
